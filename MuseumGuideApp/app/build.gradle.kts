849690cf8500f508ac84841cddbee0896df3b269
Airat Akhmadullin <airat.akhmadullin@datasentics.com>
2024-07-05 17:19:50 +0200

    Add camera libraries

--- MuseumGuideApp/app/build.gradle.kts
+++ MuseumGuideApp/app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    implementation(kotlin("script-runtime"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -66,7 +68,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    implementation (libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material.icons.extended)
+
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.video)
+    implementation(libs.androidx.camera.view.v130rc01)
+    implementation(libs.androidx.camera.extensions)
+    
 }
