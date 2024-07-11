5c39a63151e998428f6eac74958e0dd4ad97f336
Airat Akhmadullin <airat.akhmadullin@datasentics.com>
2024-07-11 15:34:39 +0200

    Integrate Camera Preview into main UI so that Camera Preview opens when middle icon is clicked

--- MuseumGuideApp/app/src/main/java/com/example/museumguide/MainActivity.kt
+++ MuseumGuideApp/app/src/main/java/com/example/museumguide/MainActivity.kt
@@ -6,13 +6,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.camera.view.CameraController
-import androidx.camera.view.LifecycleCameraController
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -23,31 +18,21 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.museumguide.data.TfLiteIClassifier
-import com.example.museumguide.domain.Classification
-import com.example.museumguide.domain.ImageAnalyzer
 import com.example.museumguide.ui.theme.MuseumGuideTheme
 
 class MainActivity : ComponentActivity() {
@@ -77,119 +62,75 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MuseumGuideApp() {
-    val context = LocalContext.current
     val navigationController = rememberNavController()
     val selectedIcon = remember {
         mutableStateOf(Icons.Default.Home)
     }
 
-    var classifications by remember {
-        mutableStateOf(emptyList<Classification>())
-    }
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                containerColor = Color.Cyan
+            ) {
+                IconButton(
+                    onClick = {
+                        selectedIcon.value = Icons.Default.Home
+                        navigationController.navigate(Screens.Home.screen) {
+                            popUpTo(0)
+                        }
+                    },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(
+                        Icons.Default.Home,
+                        contentDescription = null,
+                        modifier = Modifier.size(26.dp),
+                        tint = if (selectedIcon.value == Icons.Default.Home) Color.White else Color.DarkGray
+                    )
+                }
 
-    val analyzer = remember {
-        ImageAnalyzer (
-            classifier = TfLiteIClassifier(
-                context = context
-            ),
-            onResults = {
-                classifications = it
+                Box(modifier = Modifier
+                    .weight(1f)
+                    .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    FloatingActionButton(onClick = {
+                        navigationController.navigate(Screens.PhotoScanner.screen) {
+                            popUpTo(0)
+                        }
+                    }) {
+                        Icon(Icons.Default.Add, contentDescription = null, tint = Color.Green)
+                    }
+                }
+
+                IconButton(
+                    onClick = {
+                        selectedIcon.value = Icons.Default.PhotoLibrary
+                        navigationController.navigate(Screens.Gallery.screen) {
+                            popUpTo(0)
+                        }
+                    },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(
+                        Icons.Default.PhotoLibrary,
+                        contentDescription = null,
+                        modifier = Modifier.size(26.dp),
+                        tint = if (selectedIcon.value == Icons.Default.PhotoLibrary) Color.White else Color.DarkGray
+                    )
+                }
             }
-        )
-    }
-
-    val controller = remember {
-        LifecycleCameraController(context).apply {
-            setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
-            setImageAnalysisAnalyzer(
-                ContextCompat.getMainExecutor(context),
-                analyzer
-            )
+        }
+    ) { paddingValues ->
+        NavHost(navController = navigationController, startDestination = Screens.Home.screen,
+            modifier = Modifier.padding(paddingValues)) {
+            composable(Screens.Home.screen) { Home() }
+            composable(Screens.PhotoScanner.screen) { PhotoScanner() }
+            composable(Screens.Gallery.screen) { Gallery() }
         }
     }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        CameraPreview(controller = controller, Modifier.fillMaxSize())
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .align(Alignment.TopCenter)
-        ) {
-            classifications.forEach {
-                Text(
-                    text = it.name,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(MaterialTheme.colorScheme.primaryContainer)
-                        .padding(8.dp),
-                    textAlign = TextAlign.Center,
-                    fontSize = 20.sp,
-                    color = MaterialTheme. colorScheme.primary
-                )
-            }
-        }
-    }
-
-//    Scaffold(
-//        bottomBar = {
-//            BottomAppBar(
-//                containerColor = Color.Cyan
-//            ) {
-//                IconButton(
-//                    onClick = {
-//                        selectedIcon.value = Icons.Default.Home
-//                        navigationController.navigate(Screens.Home.screen) {
-//                            popUpTo(0)
-//                        }
-//                    },
-//                    modifier = Modifier.weight(1f)
-//                ) {
-//                    Icon(
-//                        Icons.Default.Home ,
-//                        contentDescription = null,
-//                        modifier = Modifier.size(26.dp),
-//                        tint = if (selectedIcon.value == Icons.Default.Home) Color.White else Color.DarkGray
-//                    )
-//                }
-//
-//                Box(modifier = Modifier
-//                    .weight(1f)
-//                    .padding(16.dp),
-//                    contentAlignment = Alignment.Center
-//                ) {
-//                    FloatingActionButton(onClick = { /* TODO */ }) {
-//                        Icon(Icons.Default.Add, contentDescription = null, tint = Color.Green)
-//                    }
-//                }
-//
-//                IconButton(
-//                    onClick = {
-//                        selectedIcon.value = Icons.Default.PhotoLibrary
-//                        navigationController.navigate(Screens.Gallery.screen) {
-//                            popUpTo(0)
-//                        }
-//                    },
-//                    modifier = Modifier.weight(1f)
-//                ) {
-//                    Icon(
-//                        Icons.Default.PhotoLibrary ,
-//                        contentDescription = null,
-//                        modifier = Modifier.size(26.dp),
-//                        tint = if (selectedIcon.value == Icons.Default.PhotoLibrary) Color.White else Color.DarkGray
-//                    )
-//                }
-//            }
-//        }
-//    ) { paddingValues ->
-//        NavHost(navController = navigationController, startDestination = Screens.Home.screen,
-//            modifier = Modifier.padding(paddingValues)) {
-//            composable(Screens.Home.screen) { Home() }
-//            composable(Screens.PhotoScanner.screen) { PhotoScanner() }
-//            composable(Screens.Gallery.screen) { Gallery() }
-//        }
-//    }
 }
+
 
 @Preview
 @Composable
--- MuseumGuideApp/app/src/main/java/com/example/museumguide/Photo.kt
+++ MuseumGuideApp/app/src/main/java/com/example/museumguide/Photo.kt
@@ -1,25 +1,82 @@
 package com.example.museumguide
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.example.museumguide.data.TfLiteIClassifier
+import com.example.museumguide.domain.Classification
+import com.example.museumguide.domain.ImageAnalyzer
 
 @Composable
 fun PhotoScanner() {
+    val context = LocalContext.current
+
+    var classifications by remember {
+        mutableStateOf(emptyList<Classification>())
+    }
+
+    val analyzer = remember {
+        ImageAnalyzer(
+            classifier = TfLiteIClassifier(
+                context = context
+            ),
+            onResults = {
+                classifications = it
+            }
+        )
+    }
+
+    val controller = remember {
+        LifecycleCameraController(context).apply {
+            setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
+            setImageAnalysisAnalyzer(
+                ContextCompat.getMainExecutor(context),
+                analyzer
+            )
+        }
+    }
+
     Box(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .align(Alignment.Center),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = "PhotoScanner", fontSize = 30.sp, color = Color.Black)
+        CameraPreview(controller = controller, Modifier.fillMaxSize())
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.TopCenter)
+                .padding(top = 32.dp)
+
+        ) {
+            classifications.forEach {
+                Text(
+                    text = it.name,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .padding(8.dp),
+                    textAlign = TextAlign.Center,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
         }
     }
 }
